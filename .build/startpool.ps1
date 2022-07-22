51868123efb1177d99f7947982fc15467e0acfaa
Victor.K <bauann@gmail.com>
2022-07-22 18:43:16 +0800

    腳本調整

---
+++ .build/startpool.ps1
@@ -0,0 +1,37 @@
+param 
+(
+	[string]$AppPoolName = "ActionsDemo"
+)
+
+Import-Module WebAdministration
+
+$AppPoolName = "ActionsDemo"
+$status = Get-WebAppPoolState -name $AppPoolName
+
+if ($status.Value -ne "Started") {
+    Start-WebAppPool -name $AppPoolName
+}
+else {
+    exit 0
+}
+
+$currentRetry = 0;
+$success = $false;
+do {
+    $status = Get-WebAppPoolState -name $AppPoolName
+    if ($status.Value -eq "Started") {
+        $success = $true;
+    }
+    Start-Sleep -s 5
+    $currentRetry = $currentRetry + 1;
+    write-host "Waiting for pool start:$currentRetry"
+}
+while (!$success -and $currentRetry -le 4)
+
+if ($success) {
+    write-host "start pool filed"
+    exit 0
+}
+else {
+    exit 1
+}
---
+++ .build/stoppool.ps1
@@ -0,0 +1,37 @@
+param 
+(
+	[string]$AppPoolName = "ActionsDemo"
+)
+
+Import-Module WebAdministration
+
+$AppPoolName = "ActionsDemo"
+$status = Get-WebAppPoolState -name $AppPoolName
+
+if ($status.Value -ne "Stopped") {
+    Stop-WebAppPool -name $AppPoolName
+}
+else {
+    exit 0
+}
+
+$currentRetry = 0;
+$success = $false;
+do {
+    $status = Get-WebAppPoolState -name $AppPoolName
+    if ($status.Value -eq "Stopped") {
+        $success = $true;
+    }
+    Start-Sleep -s 5
+    $currentRetry = $currentRetry + 1;
+    write-host "Waiting for pool stop:$currentRetry"
+}
+while (!$success -and $currentRetry -le 4)
+
+if ($success) {
+    write-host "stop pool filed"
+    exit 0
+}
+else {
+    exit 1
+}
